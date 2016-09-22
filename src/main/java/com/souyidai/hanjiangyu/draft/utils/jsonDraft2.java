84e839d4653a9e596f2c0cc909c1ff70a8305613
hanjiangyu <hanjiangyu@souyidai.com>
2016-09-22 18:58:53 +0800

    9.22

---
+++ src/main/java/com/souyidai/hanjiangyu/draft/utils/jsonDraft2.java
@@ -0,0 +1,25 @@
+package com.souyidai.hanjiangyu.draft.utils;
+
+import com.alibaba.fastjson.*;
+import com.souyidai.hanjiangyu.draft.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hanjiangyu on 2016/9/22.
+ */
+public class jsonDraft2 {
+    public static void main(String[] arg){
+//        String jsonString = "{\\\"code\\\":\\\"200\\\",\\\"data\\\":{\\\"ISPNUM\\\":{\\\"province\\\":\\\"北京\\\",\\\"city\\\":\\\"北京\\\",\\\"isp\\\":\\\"移动\\\"},\\\"RSL\\\":[{\\\"RS\\\":{\\\"code\\\":\\\"03\\\",\\\"desc\\\":\\\"(24,+)\\\"},\\\"IFT\\\":\\\"A3\\\"}],\\\"ECL\\\":[]},\\\"msg\\\":\\\"成功\\\"}";
+//        String  s11 = jsonString.replace("\"", "");
+//        String s22 = s11.replace("[", "");
+//        String s33 = s22.replace("]", "");
+//        String s44 = s33.replace("},{", "}");
+//        String s55 = s44.replace("\\", "");
+//        System.out.print(s55);
+//        JSONObject jsonObject = JSON.parseObject(jsonString);
+//        System.out.println(jsonObject);
+
+    }
+}

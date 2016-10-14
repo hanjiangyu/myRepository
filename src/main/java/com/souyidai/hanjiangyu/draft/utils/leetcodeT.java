04aefc99d18b23d574a81d3d60f151355b8f2dc6
hanjiangyu <hanjiangyu@souyidai.com>
2016-10-14 18:40:26 +0800

    10.14

--- src/main/java/com/souyidai/hanjiangyu/draft/utils/leetcodeT.java
+++ src/main/java/com/souyidai/hanjiangyu/draft/utils/leetcodeT.java
@@ -10,18 +10,6 @@ public class leetcodeT {
 
         String s1 = "aaaa";
         String s2 = "aaaaa";
-        //System.out.println(canConstruct(s1,s2));
 
-        String[] str1 = {"1","2"};
-        String[] str2 = {"3","4"};
-        String[] str3 = addAll(str1,str2);
-        System.out.println(str3.length);
     }
-
-    public static <T> T[] addAll(T[] first, T[] second) {
-        T[] result = Arrays.copyOf(first, first.length + second.length);
-        System.arraycopy(second, 0, result, first.length, second.length);
-        return result;
-    }
-
 }

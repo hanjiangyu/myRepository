e9b5bb52b82a2ec8480676de95139414665fcaf2
hanjiangyu <hanjiangyu@souyidai.com>
2016-11-16 18:57:49 +0800

    11.16pm

--- src/main/java/com/souyidai/hanjiangyu/draft/utils/leetcodeT.java
+++ src/main/java/com/souyidai/hanjiangyu/draft/utils/leetcodeT.java
@@ -2,15 +2,19 @@ package com.souyidai.hanjiangyu.draft.utils;
 
 import com.souyidai.hanjiangyu.draft.model.Person;
 import org.apache.commons.lang3.StringUtils;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.*;
 
 /**
  * Created by hanjiangyu on 2016/10/11.
  */
 public class leetcodeT {
-    public static void main(String[] args){
 
+    private static final Logger LOG = LoggerFactory.getLogger(leetcodeT.class);
+
+    public static void main(String[] args){
+        LOG.info("111");
         String a="";
         System.out.println(StringUtils.isNotBlank(a));
         System.out.println(a.length()>0);

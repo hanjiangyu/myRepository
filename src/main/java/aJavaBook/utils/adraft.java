9e07ab705fff530ae112ef5c905b11c729a3c16a
hanjiangyu <hanjiangyu@souyidai.com>
2017-03-10 11:57:43 +0800

    3.10

--- src/main/java/aJavaBook/utils/adraft.java
+++ src/main/java/aJavaBook/utils/adraft.java
@@ -4,11 +4,10 @@ package aJavaBook.utils;
  * Created by hanjiangyu on 2017/1/17.
  */
 public class adraft {
+    int age=1;
+
     public static void main(String[] arg){
-        double res = 1;
-        for(int i=0;i<36;i++){
-            res = res * 1.1;
-        }
-        System.out.println(res);
+        adraft a = new adraft();
+        System.out.println(a.age);
     }
 }
